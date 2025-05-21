3280effcf07ef58cbe83195b8294da2dd179cc4e
arsalan-zeus <prince.arsalang123@gmail.com>
2025-05-21 18:56:58 +0530

    bitra: let build system automatically define shipping API level.

--- device.mk
+++ device.mk
@@ -20,11 +20,6 @@ $(call inherit-product, device/realme/sm8250-common/common.mk)
 # Inherit proprietary libraries
 $(call inherit-product, vendor/realme/bitra/bitra-vendor.mk)
 
-# API
-BOARD_API_LEVEL := 30
-BOARD_SHIPPING_API_LEVEL := $(BOARD_API_LEVEL)
-PRODUCT_SHIPPING_API_LEVEL := $(BOARD_API_LEVEL)
-
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_ODM)/etc/audio_platform_info.xml \
